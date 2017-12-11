f01f4bbf136ced14479ee5198ae67192eb0b0160
Brandy <zhangzihao@zhangzihaodemac-mini.local>
2017-12-11 21:57:55 +0800

    1.0.0

--- SoHow/ViewController.swift
+++ SoHow/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let a = m
+//        test
         // Do any additional setup after loading the view, typically from a nib.
     }
 
