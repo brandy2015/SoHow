5590bfe9b4ee9721f5cebca71af43e0768f9f5c2
Brandy <zhangzihao@zhangzihaodemac-mini.local>
2017-12-11 21:13:25 +0800

    Initial Commit

---
+++ SoHow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoHow
+//
+//  Created by 张子豪 on 2017/12/11.
+//  Copyright © 2017年 张子豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
