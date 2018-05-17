ee35fe8043c6684224004e69f362d1d21c517343
Brandy <zhangzihao@zhangzihaodemac-mini.local>
2018-05-17 09:43:10 +0800

    保存

--- SoHow.podspec
+++ SoHow.podspec
@@ -46,9 +46,9 @@ Pod::Spec.new do |s|
 
   s.license      = "MIT"
 
-
-
-
+  s.swift_version = '4.0'
+ s.pod_target_xcconfig = { 'SWIFT_VERSION' => '4.0' }
+  s.ios.deployment_target = '9.1'
   # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
 
 
--- SoHow/用户名和用户数组.swift
+++ SoHow/用户名和用户数组.swift
@@ -9,27 +9,6 @@
 import UIKit
 
 
-public var 用户数组和密码 = ["丝丝大兄弟":"58ce","Fandel":"78del","Linda":"15nda","Phoebe":"37eb","default":"default","子豪":"zhangqian333","David":"vid2016","Ian":"Ian2016","Sky":"ky2016","iElaine":"ine2016","Nancy":"ac7y256","Carrie":"r0ie321","孙经淞":"sun201758"]//,"陈睿":"cheni203"]
-public var 用户数组 =  ["丝丝大兄弟","Linda","Phoebe","default","子豪","David","Ian","Sky","iElaine","Nancy","Carrie","孙经淞"]//"陈睿"
-
-
-//var 用户数组2 =
-public var 用户数组和新增评论数是否查看了 = ["丝丝大兄弟":0,"Fandel":0,"Linda":0,"Phoebe":0,"default":0,"子豪":0,"Ian":0,"David":0,"Sky":0,"iElaine":0,"Nancy":0,"Carrie":0,"陈睿":0,"孙经淞":0]
-
-
-//返回当前登录用户
-
-public func 当前登录用户() -> String{
-    let _UserName:String! = UserDefaults.standard.string(forKey: "Username")
-    if _UserName != nil{ return _UserName}else{ return "default"}
-}
-
-//判断当前是否登录
-public func 判断当前是否登录() -> Bool{
-    return 当前登录用户() != ""
-}
-
-
 
 public func b保存图片(image:UIImage,图片名称:String = "imageData",是否保存:Bool){
     if 是否保存{
