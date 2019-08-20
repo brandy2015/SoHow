09470b85e2b1321d4d51212a2a88128aa86da708
zhangqian <zhangqianbrandy2012@gmail.com>
2019-08-20 14:06:02 +0800

    version 2.0.11

---
+++ SoHow/Source/WeakProxy.swift
@@ -0,0 +1,28 @@
+//
+//  WeakProxy.swift
+//  VTLMaker
+//
+//  Created by 张子豪 on 2019/6/17.
+//  Copyright © 2019 张子豪. All rights reserved.
+//
+
+import UIKit
+
+
+public class WeakProxy: NSObject {
+    
+    weak public var target: NSObjectProtocol?
+    
+    public init(target: NSObjectProtocol) {
+        self.target = target
+        super.init()
+    }
+    
+    override public func responds(to aSelector: Selector!) -> Bool {
+        return (target?.responds(to: aSelector) ?? false) || super.responds(to: aSelector)
+    }
+    
+    override public func forwardingTarget(for aSelector: Selector!) -> Any? {
+        return target
+    }
+}
