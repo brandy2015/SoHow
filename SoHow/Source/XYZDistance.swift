e62bc4449cf91f76e7bd3605abeaf99e7ccba830
XYZTech <342994630@qq.com>
2022-07-15 20:54:57 +0800

    3.1.8
    3.1.8

---
+++ SoHow/Source/XYZDistance.swift
@@ -0,0 +1,20 @@
+//
+//  XYZDistance.swift
+//  SoHow
+//
+//  Created by zhangzihao on 2022/7/15.
+//  Copyright © 2022 张子豪. All rights reserved.
+//
+
+import UIKit
+
+//class XYZDistance: NSObject {
+//    func x(){
+//        let coordinate₀ = CLLocation(latitude: 5.0, longitude: 5.0)
+//        let coordinate₁ = CLLocation(latitude: 5.0, longitude: 4.0)
+//
+//        let distanceInMeters = coordinate₀.distance(from: coordinate₁)
+//        
+//        print(distanceInMeters/1000.0,"km")
+//    }
+//}
--- SoHow/Source/XYZNetWork.swift
+++ SoHow/Source/XYZNetWork.swift
@@ -78,3 +78,49 @@ public extension UIDevice{
           
       }
 }
+
+
+public extension UIDevice {
+    static var WIFIIP:String?{
+        return getWIFIIP()
+    }
+    /**
+     Returns device ip address. Nil if connected via celluar.
+     */
+    static func getWIFIIP() -> String? {
+        
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        
+        if getifaddrs(&ifaddr) == 0 {
+            
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next } // memory has been renamed to pointee in swift 3 so changed memory to pointee
+                
+                guard let interface = ptr?.pointee else {
+                    return nil
+                }
+                let addrFamily = interface.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                    
+                    guard let ifa_name = interface.ifa_name else {
+                        return nil
+                    }
+                    let name: String = String(cString: ifa_name)
+                    
+                    if name == "en0" {  // String.fromCString() is deprecated in Swift 3. So use the following code inorder to get the exact IP Address.
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                    
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        
+        return address
+    }
+    
+}
