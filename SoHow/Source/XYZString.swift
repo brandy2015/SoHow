d4c782102e825bcc5c508cd43073dda1bb343e00
XYZTech <10484324+brandy2015@users.noreply.github.com>
2023-08-30 20:54:46 +0800

    3.2.3
    3.2.3

--- SoHow/Source/XYZString.swift
+++ SoHow/Source/XYZString.swift
@@ -9,3 +9,25 @@
 import UIKit
 
 public extension String{ var NotEmpty:Bool{return !self.isEmpty}}
+
+public extension String{
+    
+    var isValidPhoneNumber: Bool {
+        let phoneRegex = "^1\\d{10}$"
+        let phonePredicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+        return phonePredicate.evaluate(with: self)
+    }
+    
+    var isValidEmail: Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: self)
+    }
+}
+
+public extension String {
+    var isSixDigitNumber: Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", "^\\d{6}$")
+        return predicate.evaluate(with: self)
+    }
+}
