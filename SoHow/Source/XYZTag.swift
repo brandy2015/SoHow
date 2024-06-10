eaf7925b2612936131c0039040073f0b67da45f5
XYZTech <342994630@qq.com>
2024-06-10 20:06:14 +0800

    3.2.4
    3.2.4

--- SoHow/Source/XYZTag.swift
+++ SoHow/Source/XYZTag.swift
@@ -1,33 +1,73 @@
- 
-
+// 
+//
+//import UIKit
+//
+//public extension UIViewController{
+//   
+//   func TagToUI_View(with Tag:Int) -> UIView?{
+//       return self.view.viewWithTag(Tag)
+//   }
+//   
+//   func TagToUI_TF(with Tag:Int) -> UITextField?{
+//       return self.TagToUI_View(with: Tag) as? UITextField
+//   }
+//   func TagToUI_BTN(with Tag:Int) -> UIButton?{
+//       return self.TagToUI_View(with: Tag) as? UIButton
+//   }
+//   
+//}
+//
+//
+//public extension UITableViewCell{
+//   
+//   func TagToUI_View(with Tag:Int) -> UIView?{
+//       return self.viewWithTag(Tag)
+//   }
+//   
+//   func TagToUI_TF(with Tag:Int) -> UITextField?{
+//       return self.TagToUI_View(with: Tag) as? UITextField
+//   }
+//   func TagToUI_BTN(with Tag:Int) -> UIButton?{
+//       return self.TagToUI_View(with: Tag) as? UIButton
+//   }
+//   
+//}
 import UIKit
 
-public extension UIViewController{
+public extension UIViewController {
    
-   func TagToUI_View(with Tag:Int) -> UIView?{
+   // Retrieve a UIView with the specified tag from the view controller's view hierarchy.
+   func TagToUI_View(with Tag: Int) -> UIView? {
        return self.view.viewWithTag(Tag)
    }
    
-   func TagToUI_TF(with Tag:Int) -> UITextField?{
+   // Retrieve a UITextField with the specified tag from the view controller's view hierarchy.
+   func TagToUI_TF(with Tag: Int) -> UITextField? {
        return self.TagToUI_View(with: Tag) as? UITextField
    }
-   func TagToUI_BTN(with Tag:Int) -> UIButton?{
+
+   // Retrieve a UIButton with the specified tag from the view controller's view hierarchy.
+   func TagToUI_BTN(with Tag: Int) -> UIButton? {
        return self.TagToUI_View(with: Tag) as? UIButton
    }
    
 }
 
 
-public extension UITableViewCell{
+public extension UITableViewCell {
    
-   func TagToUI_View(with Tag:Int) -> UIView?{
+   // Retrieve a UIView with the specified tag from the table view cell's view hierarchy.
+   func TagToUI_View(with Tag: Int) -> UIView? {
        return self.viewWithTag(Tag)
    }
    
-   func TagToUI_TF(with Tag:Int) -> UITextField?{
+   // Retrieve a UITextField with the specified tag from the table view cell's view hierarchy.
+   func TagToUI_TF(with Tag: Int) -> UITextField? {
        return self.TagToUI_View(with: Tag) as? UITextField
    }
-   func TagToUI_BTN(with Tag:Int) -> UIButton?{
+
+   // Retrieve a UIButton with the specified tag from the table view cell's view hierarchy.
+   func TagToUI_BTN(with Tag: Int) -> UIButton? {
        return self.TagToUI_View(with: Tag) as? UIButton
    }
    
